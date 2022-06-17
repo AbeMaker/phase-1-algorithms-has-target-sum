a8d7a518953f7574037c7754de952b0eda723bce
Abraham Anyang <anyangmaker90@gmail.com>
2022-06-17 16:46:34 +0300

    commit tree ee0afeabab0ea45ace22ace8f789d11c3f13cf7e

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers={};
+  for (const number of array){
+    const complement=target-number;
+    if(complement in seenNumbers)
+      return true;
+      seenNumbers[number]=true
+    
+    }
+    return false
+
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
 /*
+  Write the Big O time complexity of your function here
+
+  Add your pseudocode here
+
   Add written explanation of your solution here
+
+You can run `node index.js` to view these console logs
 */
 
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
@@ -31,4 +38,4 @@ if (require.main === module) {
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
-module.exports = hasTargetSum;
+module.exports = hasTargetSum
